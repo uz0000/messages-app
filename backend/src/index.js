d2fca7faf3378fcf6e28f5ea0d199d7419eb80d9
uz0000 <nwaokoro18@gmail.com>
2026-07-11 14:08:09 -0400

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import "dotenv/config"
+
+const app = express();
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => console.log("Server is running on port",PORT));
