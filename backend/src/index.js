0d71c93d137c3568091c7389f99daddc82d96e45
uz0000 <nwaokoro18@gmail.com>
2026-08-10 12:35:56 -0400

    Created user and message models, connected database, integrated Clerk middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,26 @@
 import express from "express";
-import "dotenv/config"
+import cors from "cors";
+import "dotenv/config";
+
+import { clerkMiddleware } from '@clerk/express';
+
+import User from "./models/user.model.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.listen(PORT, () => console.log("Server is running on port",PORT));
+app.use(express.json())
+app.use(cors({origin: FRONTEND_URL, credentials: true}))
+app.use(clerkMiddleware())
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ ok: true});
+});
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log("Server is running on port",PORT);
+});
