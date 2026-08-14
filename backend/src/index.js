f01a07bfec35005098261b90f33f814c527586e6
uz0000 <nwaokoro18@gmail.com>
2026-08-13 23:06:03 -0400

    Fix route handling for serving static files in production

--- backend/src/index.js
+++ backend/src/index.js
@@ -44,7 +44,7 @@ app.use("/api/messages", messageRoutes);
 if (fs.existsSync(publicDir)) {
   app.use(express.static(publicDir));
 
-  app.get("*", (req, res, next) => {
+  app.get("/*", (req, res, next) => {
     res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
   });
 }
